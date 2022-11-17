dce75f2e29707f8b3ace907ae841bf4be6b2a36a
Matthieu Fontaine <matth76330@live.fr>
2022-11-17 09:56:30 +0100

    Stage Change

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -4,10 +4,11 @@ package cicd.learn.tp1;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+
     	String unusedVariable;
     	// Comment
         System.out.println("Hello Big World!");
